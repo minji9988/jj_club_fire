f427d77a9c1c07b42c4cd116ba2ced82892c136c
user <jmj010306@gmail.com>
2023-06-08 12:47:48 +0900

    home fragment

--- app/src/main/java/com/example/jj_club/activities/home/HomeFragment.java
+++ app/src/main/java/com/example/jj_club/activities/home/HomeFragment.java
@@ -1,65 +1,94 @@
 package com.example.jj_club.activities.home;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
 
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.jj_club.R;
+import com.example.jj_club.activities.promotion.PromotionWrite1;
+import com.example.jj_club.adapters.HomeItemAdapter;
+import com.example.jj_club.models.HomeItem;
+import com.firebase.ui.firestore.FirestoreRecyclerOptions;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HomeFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HomeFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
+    private static final String TAG = "HomeFragment";
 
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
+    private RecyclerView latestPostsRecyclerView;
+    private HomeItemAdapter adapter;
 
     public HomeFragment() {
         // Required empty public constructor
     }
 
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HomeFragment.
-     */
-    // TODO: Rename and change types and number of parameters
     public static HomeFragment newInstance(String param1, String param2) {
         HomeFragment fragment = new HomeFragment();
         Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
+        args.putString("param1", param1);
+        args.putString("param2", param2);
         fragment.setArguments(args);
         return fragment;
     }
 
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_home, container, false);
+
+        setupRecyclerView(view);
+
+        ImageButton writePostButton = view.findViewById(R.id.btn_write_post);
+        writePostButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), PromotionWrite1.class);
+                startActivity(intent);
+            }
+        });
+
+        return view;
+    }
+
+    private void setupRecyclerView(View view) {
+        Query query = FirebaseFirestore.getInstance()
+                .collection("promotions")
+                .orderBy("recruitPeriod", Query.Direction.DESCENDING);
+
+        FirestoreRecyclerOptions<HomeItem> options = new FirestoreRecyclerOptions.Builder<HomeItem>()
+                .setQuery(query, HomeItem.class)
+                .build();
+
+        adapter = new HomeItemAdapter(options);
+
+        latestPostsRecyclerView = view.findViewById(R.id.recycler_view_main_page_latest);
+        latestPostsRecyclerView.setHasFixedSize(true);
+        latestPostsRecyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        latestPostsRecyclerView.setAdapter(adapter);
     }
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false);
+    public void onStart() {
+        super.onStart();
+        adapter.startListening();
+        Log.d(TAG, "Adapter started listening");
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (adapter != null) {
+            adapter.stopListening();
+            Log.d(TAG, "Adapter stopped listening");
+        }
     }
 }
--- app/src/main/java/com/example/jj_club/activities/promotion/PromotionWrite1.java
+++ app/src/main/java/com/example/jj_club/activities/promotion/PromotionWrite1.java
@@ -1,16 +1,95 @@
 package com.example.jj_club.activities.promotion;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+
+
+// 최신글 sub page making
+// 그 페이지에서 recycler view 만들고
+// 데이터 받아와서 출력하는 거 해보자.
+
+
+
+
+
 import com.example.jj_club.R;
+import com.example.jj_club.activities.home.MainActivity;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.HashMap;
+import java.util.Map;
 
 public class PromotionWrite1 extends AppCompatActivity {
+    private static final String TAG = "PromotionWrite1";
+
+    private EditText editTextTitle;
+    private EditText editTextMeetingName;
+    private EditText editTextInterview;
+    private EditText editTextFee;
+    private EditText editTextRecruitPeriod;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.promotion_write1);
+
+        editTextTitle = findViewById(R.id.editTextTitle);
+        editTextMeetingName = findViewById(R.id.editTextGroupName);
+        editTextInterview = findViewById(R.id.editTextInterview);
+        editTextFee = findViewById(R.id.editTextMembershipFee);
+        editTextRecruitPeriod = findViewById(R.id.editTextRecruitmentPeriod);
+
+        Button nextButton = findViewById(R.id.button_next);
+        nextButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+                if (user != null) {
+                    String title = editTextTitle.getText().toString();
+                    String meetingName = editTextMeetingName.getText().toString();
+                    String interview = editTextInterview.getText().toString();
+                    String fee = editTextFee.getText().toString();
+                    String recruitPeriod = editTextRecruitPeriod.getText().toString();
+
+                    if (title.isEmpty() || meetingName.isEmpty() || interview.isEmpty() || fee.isEmpty() || recruitPeriod.isEmpty()) {
+                        Toast.makeText(PromotionWrite1.this, "모든 필드를 입력해주세요.", Toast.LENGTH_SHORT).show();
+                    } else {
+                        saveToFirestore(user.getUid(), title, meetingName, interview, fee, recruitPeriod);
+                        Intent intent = new Intent(PromotionWrite1.this, MainActivity.class);
+                        startActivity(intent);
+                    }
+                } else {
+                    Toast.makeText(PromotionWrite1.this, "로그인이 필요합니다.", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+
+    private void saveToFirestore(String userId, String title, String meetingName, String interview, String fee, String recruitPeriod) {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        Map<String, Object> promotion = new HashMap<>();
+        promotion.put("userId", userId);
+        promotion.put("title", title);
+        promotion.put("meetingName", meetingName);
+        promotion.put("interview", interview);
+        promotion.put("fee", fee);
+        promotion.put("recruitPeriod", recruitPeriod);
+
+        db.collection("promotions")
+                .add(promotion)
+                .addOnSuccessListener(documentReference -> Log.d(TAG, "DocumentSnapshot added with ID: " + documentReference.getId()))
+                .addOnFailureListener(e -> Log.w(TAG, "Error adding document", e));
     }
 }
