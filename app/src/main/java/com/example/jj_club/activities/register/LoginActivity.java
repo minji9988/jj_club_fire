5c467dc4cb0cd0116af7463460451984afdbb37c
user <jmj010306@gmail.com>
2023-06-10 14:29:04 +0900

    commend login and sign up

--- app/src/main/java/com/example/jj_club/activities/register/LoginActivity.java
+++ app/src/main/java/com/example/jj_club/activities/register/LoginActivity.java
@@ -17,40 +17,45 @@ import com.example.jj_club.R;
 import com.example.jj_club.activities.home.MainActivity;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseAuthException;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
 
 public class LoginActivity extends AppCompatActivity {
 
+    // 디버그 메시지를 로깅하기 위한 태그
     private static final String TAG = "LoginActivity";
 
+    // Firebase Authentication 인스턴스
     private FirebaseAuth mAuth;
 
+    // UI 요소
     private EditText emailEditText, passwordEditText;
     private Button loginButton;
     private TextView registerTextButton;
 
-    //private DatabaseReference databaseReference;
+    // 데이터베이스 참조 (주석 처리됨)
+    // private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_login);
 
+        // Firebase Authentication 초기화
         mAuth = FirebaseAuth.getInstance();
-        //databaseReference = FirebaseDatabase.getInstance().getReference();
 
+        // UI 요소를 변수에 할당
         emailEditText = findViewById(R.id.editText_email_login);
         passwordEditText = findViewById(R.id.editText_password);
         loginButton = findViewById(R.id.button_login);
         registerTextButton = findViewById(R.id.registerTextButton);
 
+        // 로그인 버튼에 클릭 리스너 설정
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String email = emailEditText.getText().toString();
                 String password = passwordEditText.getText().toString();
 
+                // 이메일과 비밀번호가 비어있는지 확인
                 if (TextUtils.isEmpty(email) || TextUtils.isEmpty(password)) {
                     if (TextUtils.isEmpty(email)) {
                         emailEditText.setHintTextColor(Color.RED);
@@ -60,6 +65,7 @@ public class LoginActivity extends AppCompatActivity {
                     }
                     Toast.makeText(LoginActivity.this, "모든 입력란을 채워주세요.", Toast.LENGTH_SHORT).show();
                 } else {
+                    // Firebase Authentication을 사용하여 로그인 시도
                     mAuth.signInWithEmailAndPassword(email, password)
                             .addOnCompleteListener(LoginActivity.this, task -> {
                                 if (task.isSuccessful()) {
@@ -77,6 +83,7 @@ public class LoginActivity extends AppCompatActivity {
             }
         });
 
+        // 회원가입 텍스트 버튼에 클릭 리스너 설정
         registerTextButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -86,6 +93,7 @@ public class LoginActivity extends AppCompatActivity {
         });
     }
 
+    // Firebase Authentication 예외 메시지 가져오기
     private String getFirebaseAuthErrorMessage(Exception exception) {
         if (exception instanceof FirebaseAuthException) {
             FirebaseAuthException authException = (FirebaseAuthException) exception;
--- app/src/main/java/com/example/jj_club/activities/register/SignUpActivity.java
+++ app/src/main/java/com/example/jj_club/activities/register/SignUpActivity.java
@@ -36,43 +36,56 @@ public class SignUpActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.register_sign_up);
 
+        // Firebase Authentication 인스턴스 초기화
         mAuth = FirebaseAuth.getInstance();
+        // Firebase Realtime Database의 "users" 노드에 대한 참조 가져오기
         mDatabase = FirebaseDatabase.getInstance().getReference().child("users");
 
-        nameEditText = findViewById(R.id.nameEditText);
-        phoneNumberEditText = findViewById(R.id.phoneNumberEditText);
-        passwordEditText = findViewById(R.id.passwordEditText);
-        recheckPasswordEditText = findViewById(R.id.recheckPasswordEditText);
-        signUpButton = findViewById(R.id.signUpButton);
+        // UI 요소 변수에 할당
+        nameEditText = findViewById(R.id.nameEditText);  // 닉네임
+        phoneNumberEditText = findViewById(R.id.phoneNumberEditText); // 전화번호
+        passwordEditText = findViewById(R.id.passwordEditText); // 비밀번호
+        recheckPasswordEditText = findViewById(R.id.recheckPasswordEditText); // 비밀번호 다시 입력란
+        signUpButton = findViewById(R.id.signUpButton); // 회원가입 버튼
 
+        // 회원가입 버튼에 클릭 리스너 설정
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                // 사용자가 입력한 정보 가져오기
                 String name = nameEditText.getText().toString();
                 String phoneNumber = phoneNumberEditText.getText().toString();
                 String password = passwordEditText.getText().toString();
                 String recheckPassword = recheckPasswordEditText.getText().toString();
 
+                // 비밀번호가 6자 이상인지 확인
                 if (password.length() >= 6) {
+                    // 비밀번호와 비밀번호 확인이 일치하는지 확인
                     if (password.equals(recheckPassword)) {
-                        FirebaseUser user = mAuth.getCurrentUser(); //회원가입된 유저 가져온다?
+                        // 현재 인증된 사용자 가져오기
+                        FirebaseUser user = mAuth.getCurrentUser();
                         if (user != null) {
+                            // 비밀번호 업데이트 시도
                             user.updatePassword(password)
                                     .addOnCompleteListener(new OnCompleteListener<Void>() {
                                         @Override
                                         public void onComplete(@NonNull Task<Void> task) {
                                             if (task.isSuccessful()) {
+                                                // 사용자 정보를 HashMap에 저장
                                                 Map<String, Object> userInfo = new HashMap<>();
                                                 userInfo.put("email", user.getEmail());
                                                 userInfo.put("name", name);
                                                 userInfo.put("phoneNumber", phoneNumber);
 
+                                                // Firebase Realtime Database에 사용자 정보 저장
                                                 mDatabase.child(user.getUid()).setValue(userInfo)
                                                         .addOnSuccessListener(new OnSuccessListener<Void>() {
                                                             @Override
                                                             public void onSuccess(Void aVoid) {
+                                                                // 회원가입 성공 메시지 출력
                                                                 Toast.makeText(SignUpActivity.this, "회원가입 성공", Toast.LENGTH_SHORT).show();
 
+                                                                // 로그인 화면으로 이동
                                                                 Intent intent = new Intent(SignUpActivity.this, LoginActivity.class);
                                                                 startActivity(intent);
                                                                 finish();
@@ -81,21 +94,26 @@ public class SignUpActivity extends AppCompatActivity {
                                                         .addOnFailureListener(new OnFailureListener() {
                                                             @Override
                                                             public void onFailure(@NonNull Exception e) {
+                                                                // 회원가입 실패 메시지 출력
                                                                 Toast.makeText(SignUpActivity.this, "회원가입 실패: " + e.getMessage(), Toast.LENGTH_LONG).show();
                                                             }
                                                         });
                                             } else {
+                                                // 비밀번호 업데이트 실패 메시지 출력
                                                 Toast.makeText(SignUpActivity.this, "비밀번호 업데이트 실패: " + task.getException().getMessage(), Toast.LENGTH_SHORT).show();
                                             }
                                         }
                                     });
                         } else {
+                            // 인증된 사용자가 아닌 경우 메시지 출력
                             Toast.makeText(SignUpActivity.this, "인증된 사용자가 아닙니다.", Toast.LENGTH_SHORT).show();
                         }
                     } else {
+                        // 비밀번호 일치하지 않는 경우 메시지 출력
                         Toast.makeText(SignUpActivity.this, "비밀번호가 일치하지 않습니다.", Toast.LENGTH_SHORT).show();
                     }
                 } else {
+                    // 비밀번호가 6자 이상이 아닌 경우 메시지 출력
                     Toast.makeText(SignUpActivity.this, "비밀번호를 6자 이상 입력해야 합니다.", Toast.LENGTH_SHORT).show();
                 }
             }
