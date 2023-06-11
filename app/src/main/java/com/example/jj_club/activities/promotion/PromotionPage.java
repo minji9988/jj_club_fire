e91c6074ba70531bd51af831a265570b0475a51c
wlgur313 <wlgur313@naver.com>
2023-06-11 16:10:58 +0900

    ApplicationForm 2306111610

--- app/src/main/java/com/example/jj_club/activities/promotion/PromotionPage.java
+++ app/src/main/java/com/example/jj_club/activities/promotion/PromotionPage.java
@@ -1,13 +1,13 @@
 package com.example.jj_club.activities.promotion;
-
-// PromotionPage.java
-
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.jj_club.R;
 import com.example.jj_club.fragments.PromotionPageInfoFragment;
@@ -22,6 +22,17 @@ public class PromotionPage extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.promotion_page);
 
+        // button_application에 클릭 리스너 추가
+        Button applicationButton = findViewById(R.id.button_application);
+        applicationButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // ApplicationForm 액티비티로 이동
+                Intent intent = new Intent(PromotionPage.this, ApplicationForm.class);
+                startActivity(intent);
+            }
+        });
+
         TabLayout tabLayout = findViewById(R.id.tap);
         tabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
@@ -29,16 +40,16 @@ public class PromotionPage extends AppCompatActivity {
                 FragmentManager fragmentManager = getSupportFragmentManager();
                 FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
 
-                switch (tab.getId()) {
-                    case R.id.tab_info:
+                switch (tab.getPosition()) {
+                    case 0:
                         PromotionPageInfoFragment infoFragment = new PromotionPageInfoFragment();
                         fragmentTransaction.replace(R.id.information_tap, infoFragment);
                         break;
-                    case R.id.tab_reviews:
+                    case 1:
                         PromotionPageReviewsFragment reviewsFragment = new PromotionPageReviewsFragment();
                         fragmentTransaction.replace(R.id.information_tap, reviewsFragment);
                         break;
-                    case R.id.tab_qa:
+                    case 2:
                         PromotionPageQAFragment qaFragment = new PromotionPageQAFragment();
                         fragmentTransaction.replace(R.id.information_tap, qaFragment);
                         break;
@@ -51,12 +62,12 @@ public class PromotionPage extends AppCompatActivity {
 
             @Override
             public void onTabUnselected(TabLayout.Tab tab) {
-                // Handle tab unselected event if needed
+                // 필요한 경우 탭 선택 취소 이벤트 처리
             }
 
             @Override
             public void onTabReselected(TabLayout.Tab tab) {
-                // Handle tab reselected event if needed
+                // 필요한 경우 탭 다시 선택 이벤트 처리
             }
         });
     }
