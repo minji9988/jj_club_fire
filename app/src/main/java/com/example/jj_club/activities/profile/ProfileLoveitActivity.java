808e3a226edcfb43e56ddce46c38d3c8839b010e
Dongsu Lee <dongsu0717@nate.com>
2023-06-11 23:17:52 +0900

    HomeItemAdapter2 = 찜목록 어뎁터........

--- app/src/main/java/com/example/jj_club/activities/profile/ProfileLoveitActivity.java
+++ app/src/main/java/com/example/jj_club/activities/profile/ProfileLoveitActivity.java
@@ -4,24 +4,18 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import com.example.jj_club.R;
 import com.example.jj_club.adapters.HomeItemAdapter2;
-import com.example.jj_club.adapters.LoveitAdapter;
 import com.example.jj_club.models.HomeItem;
-import com.example.jj_club.models.LoveitItem;
 import com.firebase.ui.database.FirebaseRecyclerOptions;
 import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.Query;
-import com.google.firebase.database.ValueEventListener;
 
 import java.util.ArrayList;
 
