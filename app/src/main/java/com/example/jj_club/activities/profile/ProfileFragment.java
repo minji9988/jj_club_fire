2868c32d52c48e2f1ea1a7ac431996181b49869a
user <jmj010306@gmail.com>
2023-06-12 11:42:14 +0900

    0612-11:42- Modify profileFragment, loveitActivity

--- app/src/main/java/com/example/jj_club/activities/profile/ProfileFragment.java
+++ app/src/main/java/com/example/jj_club/activities/profile/ProfileFragment.java
@@ -4,16 +4,14 @@ import android.app.AlertDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
-
-import androidx.fragment.app.Fragment;
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.TextView;
-import android.widget.Toast;
+
+import androidx.fragment.app.Fragment;
 
 import com.example.jj_club.R;
 import com.example.jj_club.activities.register.LoginActivity;
